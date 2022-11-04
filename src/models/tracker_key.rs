8d26faa76ab11dbebcd6e25ee5a4b7414592dfb1
Jose Celano <josecelano@gmail.com>
2022-11-04 10:27:42 +0000

    fix: [#78] parsing keys from tracker

--- src/models/tracker_key.rs
+++ src/models/tracker_key.rs
@@ -6,3 +6,15 @@ pub struct TrackerKey {
     pub key: String,
     pub valid_until: i64,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct NewTrackerKey {
+    pub key: String,
+    pub valid_until: Duration,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Duration {
+    pub secs: i64,
+    pub nanos: i64,
+}
