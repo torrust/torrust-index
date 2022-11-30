728fe8a17e090b56dbde3284c22f3956b641edbd
Jose Celano <josecelano@gmail.com>
2022-11-30 16:40:23 +0000

    fix: [#78] remove unused struct NewTrackerKey
    It was added because the tracker API was changed, but that change was
    reverted here:
    
    https://github.com/torrust/torrust-tracker/pull/109
    
    and it is not used anymore.

--- src/models/tracker_key.rs
+++ src/models/tracker_key.rs
@@ -8,12 +8,6 @@ pub struct TrackerKey {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct NewTrackerKey {
-    pub key: String,
-    pub valid_until: Duration,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
 pub struct Duration {
     pub secs: i64,
     pub nanos: i64,
