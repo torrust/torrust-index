b53ce8d3f3b90720077867ac5510d63f6b0cde09
Jose Celano <josecelano@gmail.com>
2023-06-15 13:19:11 +0100

    refactor(api): [#198] Axum API, tag context

--- src/web/api/v1/contexts/mod.rs
+++ src/web/api/v1/contexts/mod.rs
@@ -6,6 +6,7 @@
 //! `Category` | Torrent categories | [`v1`](crate::web::api::v1::contexts::category)
 //! `Proxy` | Image proxy cache | [`v1`](crate::web::api::v1::contexts::proxy)
 //! `Settings` | Index settings | [`v1`](crate::web::api::v1::contexts::settings)
+//! `Tag` | Torrent tags | [`v1`](crate::web::api::v1::contexts::tag)
 //! `Torrent` | Indexed torrents | [`v1`](crate::web::api::v1::contexts::torrent)
 //! `User` | Users | [`v1`](crate::web::api::v1::contexts::user)
 //!
@@ -13,5 +14,6 @@ pub mod about;
 pub mod category;
 pub mod proxy;
 pub mod settings;
+pub mod tag;
 pub mod torrent;
 pub mod user;
