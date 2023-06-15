878bb7b3b55d40b21549e0bdfa706595311a3792
Jose Celano <josecelano@gmail.com>
2023-06-15 12:36:00 +0100

    refactor(api): [#198] Axum API, root endpoints

--- src/web/api/v1/routes.rs
+++ src/web/api/v1/routes.rs
@@ -1,22 +1,27 @@
 //! Route initialization for the v1 API.
 use std::sync::Arc;
 
+use axum::routing::get;
 use axum::Router;
 
 //use tower_http::cors::CorsLayer;
 use super::contexts::about;
+use super::contexts::about::handlers::about_page_handler;
 use super::contexts::{category, user};
 use crate::common::AppData;
 
 /// Add all API routes to the router.
 #[allow(clippy::needless_pass_by_value)]
 pub fn router(app_data: Arc<AppData>) -> Router {
-    let api_routes = Router::new()
+    let v1_api_routes = Router::new()
+        .route("/", get(about_page_handler).with_state(app_data.clone()))
         .nest("/user", user::routes::router(app_data.clone()))
         .nest("/about", about::routes::router(app_data.clone()))
-        .nest("/category", category::routes::router(app_data));
+        .nest("/category", category::routes::router(app_data.clone()));
 
-    Router::new().nest("/v1", api_routes)
+    Router::new()
+        .route("/", get(about_page_handler).with_state(app_data))
+        .nest("/v1", v1_api_routes)
 
     // For development purposes only.
     // It allows calling the API on a different port. For example
