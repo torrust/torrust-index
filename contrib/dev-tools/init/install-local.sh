902e8c7e2f505ee8227b0f2ac055279172825933
Jose Celano <josecelano@gmail.com>
2023-11-29 16:47:45 +0000

    chore: add script to setup DB for development

---
+++ contrib/dev-tools/init/install-local.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script is only intended to be used for development environment.
+
+# Generate the Index sqlite database directory and file if it does not exist
+mkdir -p ./storage/index/lib/database
+
+if ! [ -f "./storage/index/lib/database/sqlite3.db.db" ]; then
+    echo "Creating index database 'sqlite3.db.db'"
+    sqlite3 "./storage/index/lib/database/sqlite3.db.db" "VACUUM;"
+fi
