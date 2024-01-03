7620ed1c59bc64f3e9a8f9c162abecfc578537cd
Jose Celano <josecelano@gmail.com>
2024-01-03 14:03:16 +0000

    chore: disable clippy error until we fix it
    Relates to: https://github.com/torrust/torrust-index/issues/388

--- tests/common/contexts/torrent/responses.rs
+++ tests/common/contexts/torrent/responses.rs
@@ -74,17 +74,9 @@ pub struct TorrentDetails {
     pub encoding: Option<String>,
 }
 
-#[rustversion::stable]
-#[derive(Deserialize, PartialEq, Debug)]
-pub struct Category {
-    pub category_id: CategoryId, // todo: rename to `id`
-    pub name: String,
-    pub num_torrents: u64,
-}
-
-#[rustversion::nightly]
-#[derive(Deserialize, PartialEq, Debug)]
+#[allow(unknown_lints)]
 #[allow(clippy::struct_field_names)]
+#[derive(Deserialize, PartialEq, Debug)]
 pub struct Category {
     pub category_id: CategoryId, // todo: rename to `id`
     pub name: String,
