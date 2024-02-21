8f7b8a68ab46b341e358717be9dc904e9aa1a858
Mario <mariomnt945@gmail.com>
2024-02-21 16:39:47 +0100

    feat: [#446] new optional user id extractor

--- src/web/api/server/v1/extractors/mod.rs
+++ src/web/api/server/v1/extractors/mod.rs
@@ -1,2 +1,3 @@
 pub mod bearer_token;
 pub mod user_id;
+pub mod optional_user_id;
---
+++ src/web/api/server/v1/extractors/optional_user_id.rs
@@ -0,0 +1,41 @@
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use axum::extract::{FromRef, FromRequestParts};
+use axum::http::request::Parts;
+use axum::response::{ Response};
+
+use crate::common::AppData;
+use crate::models::user::UserId;
+use crate::web::api::server::v1::extractors::bearer_token;
+
+pub struct ExtractOptionalLoggedInUser(pub Option<UserId>);
+
+#[async_trait]
+impl<S> FromRequestParts<S> for ExtractOptionalLoggedInUser
+    where
+        Arc<AppData>: FromRef<S>,
+        S: Send + Sync,
+{
+    type Rejection = Response;
+
+    async fn from_request_parts(parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+       /* let maybe_bearer_token = match bearer_token::Extract::from_request_parts(parts, state).await {
+            Ok(maybe_bearer_token) => maybe_bearer_token.0,
+            Err(_) => return Err(ServiceError::TokenNotFound.into_response()),
+        }; */
+
+        let bearer_token = match  bearer_token::Extract::from_request_parts(parts, state).await {
+          Ok(bearer_token)  => bearer_token.0,
+            Err(_) => None
+        };
+
+        //Extracts the app state
+        let app_data = Arc::from_ref(state);
+
+        match app_data.auth.get_user_id_from_bearer_token(&bearer_token).await {
+            Ok(user_id) => Ok(ExtractOptionalLoggedInUser(Some(user_id))),
+            Err(_) => Ok(ExtractOptionalLoggedInUser(None)),
+        }
+    }
+}
