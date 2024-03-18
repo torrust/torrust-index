81f68ffdd03a6228f8abb508a399e762cf9c7bc1
Mario <mariomnt945@gmail.com>
2024-03-18 23:09:44 +0100

    feat: [#448] authorization logic now done by the authorization service

--- src/services/tag.rs
+++ src/services/tag.rs
@@ -1,7 +1,7 @@
 //! Tag service.
 use std::sync::Arc;
 
-use super::user::DbUserRepository;
+use super::authorization::AuthorizationService;
 use crate::databases::database::{Database, Error as DatabaseError, Error};
 use crate::errors::ServiceError;
 use crate::models::torrent_tag::{TagId, TorrentTag};
@@ -9,15 +9,15 @@ use crate::models::user::UserId;
 
 pub struct Service {
     tag_repository: Arc<DbTagRepository>,
-    user_repository: Arc<DbUserRepository>,
+    authorization_service: Arc<AuthorizationService>,
 }
 
 impl Service {
     #[must_use]
-    pub fn new(tag_repository: Arc<DbTagRepository>, user_repository: Arc<DbUserRepository>) -> Service {
+    pub fn new(tag_repository: Arc<DbTagRepository>, authorization_service: Arc<AuthorizationService>) -> Service {
         Service {
             tag_repository,
-            user_repository,
+            authorization_service,
         }
     }
 
@@ -30,13 +30,7 @@ impl Service {
     /// * The user does not have the required permissions.
     /// * There is a database error.
     pub async fn add_tag(&self, tag_name: &str, user_id: &UserId) -> Result<TagId, ServiceError> {
-        let user = self.user_repository.get_compact(user_id).await?;
-
-        // Check if user is administrator
-        // todo: extract authorization service
-        if !user.administrator {
-            return Err(ServiceError::Unauthorized);
-        }
+        self.authorization_service.authorize_user(*user_id, true).await?;
 
         let trimmed_name = tag_name.trim();
 
@@ -62,13 +56,7 @@ impl Service {
     /// * The user does not have the required permissions.
     /// * There is a database error.
     pub async fn delete_tag(&self, tag_id: &TagId, user_id: &UserId) -> Result<(), ServiceError> {
-        let user = self.user_repository.get_compact(user_id).await?;
-
-        // Check if user is administrator
-        // todo: extract authorization service
-        if !user.administrator {
-            return Err(ServiceError::Unauthorized);
-        }
+        self.authorization_service.authorize_user(*user_id, true).await?;
 
         match self.tag_repository.delete(tag_id).await {
             Ok(()) => Ok(()),
