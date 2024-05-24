56a8c08a8c95585c476419705e9e80820ff4dad6
Jose Celano <josecelano@gmail.com>
2024-05-24 12:12:51 +0100

    feat: [#603] add default values to configration

--- src/config/v1/api.rs
+++ src/config/v1/api.rs
@@ -4,16 +4,28 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct Api {
     /// The default page size for torrent lists.
+    #[serde(default = "Api::default_default_torrent_page_size")]
     pub default_torrent_page_size: u8,
     /// The maximum page size for torrent lists.
+    #[serde(default = "Api::default_max_torrent_page_size")]
     pub max_torrent_page_size: u8,
 }
 
 impl Default for Api {
     fn default() -> Self {
         Self {
-            default_torrent_page_size: 10,
-            max_torrent_page_size: 30,
+            default_torrent_page_size: Api::default_default_torrent_page_size(),
+            max_torrent_page_size: Api::default_max_torrent_page_size(),
         }
+    }
+}
+
+impl Api {
+    fn default_default_torrent_page_size() -> u8 {
+        10
+    }
+
+    fn default_max_torrent_page_size() -> u8 {
+        30
     }
 }
--- src/config/v1/database.rs
+++ src/config/v1/database.rs
@@ -8,13 +8,20 @@ pub struct Database {
     ///
     /// Sqlite: `sqlite://data.db?mode=rwc`.
     /// Mysql: `mysql://root:root_secret_password@mysql:3306/torrust_index_e2e_testing`.
+    #[serde(default = "Database::default_connect_url")]
     pub connect_url: Url,
 }
 
 impl Default for Database {
     fn default() -> Self {
         Self {
-            connect_url: Url::parse("sqlite://data.db?mode=rwc").unwrap(),
+            connect_url: Self::default_connect_url(),
         }
+    }
+}
+
+impl Database {
+    fn default_connect_url() -> Url {
+        Url::parse("sqlite://data.db?mode=rwc").unwrap()
     }
 }
