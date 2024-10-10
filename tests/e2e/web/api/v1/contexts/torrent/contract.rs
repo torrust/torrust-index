4c094db2e0857fa4b034a0f7994ff3b0fd9195cd
Mario <mariomnt945@gmail.com>
2024-10-10 14:05:09 +0200

    test: new test for admin users to delete torrents

--- tests/e2e/web/api/v1/contexts/torrent/contract.rs
+++ tests/e2e/web/api/v1/contexts/torrent/contract.rs
@@ -1368,7 +1368,13 @@ mod and_admins {
                 client::Client,
                 contexts::torrent::{fixtures::random_torrent, forms::UploadTorrentMultipartForm},
             },
-            e2e::{environment::TestEnv, web::api::v1::contexts::user::steps::new_logged_in_admin},
+            e2e::{
+                environment::TestEnv,
+                web::api::v1::contexts::{
+                    torrent::steps::upload_random_torrent_to_index,
+                    user::steps::{new_logged_in_admin, new_logged_in_user},
+                },
+            },
         };
 
         #[tokio::test]
@@ -1389,6 +1395,27 @@ mod and_admins {
             let form: UploadTorrentMultipartForm = test_torrent.index_info.into();
 
             let response = client.upload_torrent(form.into()).await;
+
+            assert_eq!(response.status, 200);
+        }
+
+        #[tokio::test]
+        async fn it_should_allow_admin_users_to_delete_torrents() {
+            let mut env = TestEnv::new();
+            env.start(api::Version::V1).await;
+
+            if !env.provides_a_tracker() {
+                println!("test skipped. It requires a tracker to be running.");
+                return;
+            }
+
+            let uploader = new_logged_in_user(&env).await;
+            let (test_torrent, _uploaded_torrent) = upload_random_torrent_to_index(&uploader, &env).await;
+
+            let admin = new_logged_in_admin(&env).await;
+            let client = Client::authenticated(&env.server_socket_addr().unwrap(), &admin.token);
+
+            let response = client.delete_torrent(&test_torrent.file_info_hash()).await;
 
             assert_eq!(response.status, 200);
         }
